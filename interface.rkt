bf263d81716929544920ce60237f3f0b8b228aaa
Jay McCarthy <jay.mccarthy@gmail.com>
2012-09-24 12:58:26 -0600

    syntax/parse

--- interface.rkt
+++ interface.rkt
@@ -1,21 +1,22 @@
 #lang racket/base
 (require racket/stxparam
-         (for-syntax racket/base
+         (for-syntax syntax/parse
+                     racket/base
                      racket/function
                      syntax/strip-context
                      racket/syntax))
 
 (begin-for-syntax
-  (struct interface-info (dyn param static dict fields)))
+  (struct interface-info (dyn param static dict members)))
 
 (define-syntax (define-interface stx)
-  (syntax-case stx ()
-    [(_ id (f ...))
+  (syntax-parse stx
+    [(_ interface:id (member:id ...))
      (with-syntax*
-      ([s (generate-temporary #'id)]
-       [(s-f ...)
-        (for/list ([fid (in-list (syntax->list #'(f ...)))])
-          (format-id #'s "~a-~a" #'s fid))])
+      ([dict (generate-temporary #'interface)]
+       [(dict-member ...)
+        (for/list ([mid (in-list (syntax->list #'(member ...)))])
+          (format-id #'dict "~a-~a" #'dict mid))])
       (syntax/loc stx
         (begin
           (define dynamic-id-param (make-parameter #f))
@@ -24,56 +25,59 @@
 
           (define-syntax-parameter static-id
             (make-rename-transformer #'dynamic-id))
-          (define-syntax id
+          (define-syntax interface
             (interface-info #'dynamic-id #'dynamic-id-param #'static-id
-                            #'s
-                            #'(f ...)))
+                            #'dict
+                            #'(member ...)))
 
-          (struct s (f ...))
+          (struct dict (member ...))
 
-          (define-syntax-rule (f . e)
-            ((s-f static-id) . e))
+          (define-syntax-rule (member . e)
+            ((dict-member static-id) . e))
           ...)))]))
 
 (define-syntax (with-generics stx)
-  (syntax-case stx ()
-    [(_ interface instance . body)
+  (syntax-parse stx
+    [(_ interface instance:id . body:expr)
+     #:declare interface (static interface-info? "interface")
      (quasisyntax/loc stx
        (let ([instance-id instance])
          (syntax-parameterize
-             ([#,(interface-info-static (syntax-local-value #'interface))
+             ([#,(interface-info-static (attribute interface.value))
                (make-rename-transformer #'instance-id)])
            (parameterize
-               ([#,(interface-info-param (syntax-local-value #'interface))
+               ([#,(interface-info-param (attribute interface.value))
                  instance-id])
              . body))))]))
 
 (define-syntax (generalized stx)
-  (syntax-case stx ()
-    [(_ interface . body)
+  (syntax-parse stx
+    [(_ interface . body:expr)
+     #:declare interface (static interface-info? "interface")
      (quasisyntax/loc stx
        (syntax-parameterize
-           ([#,(interface-info-static (syntax-local-value #'interface))
+           ([#,(interface-info-static (attribute interface.value))
              (make-rename-transformer
-              #'#,(interface-info-dyn (syntax-local-value #'interface)))])
+              #'#,(interface-info-dyn (attribute interface.value)))])
          . body))]))
 
 (define-syntax (define-instance stx)
-  (syntax-case stx ()
-    [(_ interface instance body ...)
+  (syntax-parse stx
+    [(_ interface instance:id body:expr ...)
+     #:declare interface (static interface-info? "interface")
      (with-syntax
-         ([s
-           (interface-info-dict (syntax-local-value #'interface))]
-          [(f ...)
+         ([dict
+           (interface-info-dict (attribute interface.value))]
+          [(member ...)
            (map
             (curry replace-context #'instance)
             (syntax->list
-             (interface-info-fields (syntax-local-value #'interface))))])
+             (interface-info-members (attribute interface.value))))])
        (syntax/loc stx
          (define instance
            (let ()
              body ...
-             (s f ...)))))]))
+             (dict member ...)))))]))
 
 (provide
  define-interface
